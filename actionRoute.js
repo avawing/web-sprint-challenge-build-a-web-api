ca50c82cd5533443ca6fafbb7dbd4c3a61576581
Aveline Wingfield <avelinewingfield@gmail.com>
2020-09-04 12:41:31 -0400

    Middleware added for verification on actions

--- actionRoute.js
+++ actionRoute.js
@@ -1,45 +1,51 @@
-const express = require("express")
+const express = require("express");
 const actionsDb = require("./data/helpers/actionModel");
-const actionRouter = express.Router()
+const actionRouter = express.Router();
 
+function validateId(req, res, next) {
+  actionsDb.Db.get(req.params.id)
+    .then((result) => next())
+    .catch((err) => res.status(404).json({ message: "Action not found" }))
+    .end();
+}
 
+actionRouter.get("/:id", validateId, (req, res, next) => {
+  actionsDb
+    .get(req.params.id)
+    .then((action) => res.status(200).json(action).end())
+    .catch((err) => res.status(500).json({ message: "Server fucked up" }))
+    .end();
+});
 
-actionRouter.get("/:id", (req, res, next) => {
+actionRouter.put("/:id", validateId, (req, res, next) => {
+  if (
+    req.body.project_id === "" ||
+    req.body.description === "" ||
+    req.body.notes === ""
+  ) {
+    res.status(400).json({ message: "Please fill all fields" }).end();
+  } else {
     actionsDb
-      .get(req.params.id)
-      .then((action) =>
-        action
-          ? res.status(200).json(action)
-          : res.status(404).json({ message: "Action not found" }).end()
+      .update(req.params.id, req.body)
+      .then((response) => res.status(200).json(response).end())
+      .catch((err) =>
+        res.status(500).json({ message: "Unable to update" }).end()
       );
-  });
+  }
+});
 
-  actionRouter.put("/:id", (req, res, next) => {
-    actionsDb.get(req.params.id).then((result) => {
-      result
-        ? actionsDb
-            .update(req.params.id, req.body)
-            .then((response) =>
-              response
-                ? res.status(200).json(response).end()
-                : res.status(500).json({ message: "Unable to update" }).end()
-            )
-        : res.status(404).json({ message: "action not found" }).end();
-    });
+actionRouter.delete("/:id", (req, res, next) => {
+  actionsDb.get(req.params.id).then((result) => {
+    result
+      ? actionsDb
+          .remove(req.params.id)
+          .then((result) =>
+            result === 1
+              ? res.status(204).json({ message: "Item removed" }).end()
+              : res.status(500).json({ message: "Something went wrong" }).end()
+          )
+      : res.status(404).json({ message: "Not Found" }).end();
   });
-
-  actionRouter.delete('/:id',(req, res, next)=>{
-    actionsDb.get(req.params.id).then((result) => {
-        result
-          ? actionsDb
-              .remove(req.params.id)
-              .then((result) =>
-                result === 1
-                  ? res.status(204).json({message: "Item removed"}).end()
-                  : res.status(500).json({ message: "Something went wrong" }).end()
-              )
-          : res.status(404).json({ message: "Not Found" }).end();
-      });
-})
+});
 
 module.exports = actionRouter;
