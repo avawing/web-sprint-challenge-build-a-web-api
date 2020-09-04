9eb2f7e09de1c2a4484a1ed22d979efce5df26b6
Aveline Wingfield <avelinewingfield@gmail.com>
2020-09-04 11:45:40 -0400

    Gets and Posts working

--- index.js
+++ index.js
@@ -1,3 +1,72 @@
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+
+const projectDb = require("./data/helpers/projectModel");
+const actionsDb = require("./data/helpers/actionModel");
+
+const server = express();
+
+server.use(express.json());
+server.use("/", morgan("tiny"));
+server.use("/", helmet());
+
+server.get("/", (req, res, next) => {
+  res.send("Hi");
+});
+
+server.get("/projects/:id", (req, res, next) => {
+  projectDb.get(req.params.id).then((result) => {
+    result
+      ? res.status(200).json(result)
+      : res.status(404).json({ message: "Not Found" });
+  });
+});
+
+server.get("/projects/:p_id/actions/:a_id", (req, res, next) => {
+  projectDb.get(req.params.p_id).then((project) => {
+    project
+      ? actionsDb
+          .get(req.params.a_id)
+          .then((action) =>
+            action
+              ? res.status(200).json(action)
+              : res.status(404).json({ message: "Action not found" })
+          )
+      : res.status(404).json({ message: "Project not found" });
+  });
+});
+
+server.post("/projects", (req, res, next) => {
+  projectDb
+    .insert(req.body)
+    .then((response) =>
+      response
+        ? res.status(201).json(response)
+        : res.status(500).json({ message: "There was an error" })
+    );
+});
+
+server.post("/projects/:id/actions", (req, res, next) => {
+  const actions = req.body;
+  actions.project_id = req.params.id;
+  projectDb.get(req.params.p_id).then((project) => {
+    project
+      ? actionsDb
+          .insert(actions)
+          .then((action) =>
+            action
+              ? res.status(201).json(action)
+              : res.status(500).json({ message: "There was a problem" })
+          )
+      : res.status(404).json({ message: "Project not found" });
+  });
+});
+
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => console.log(`server is listening on ${port}`));
+
 /*
 play this: https://www.youtube.com/watch?v=d-diB65scQU
 
