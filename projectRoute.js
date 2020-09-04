fcea6098b808105603eb0917f33a9b87dce52fde
Aveline Wingfield <avelinewingfield@gmail.com>
2020-09-04 12:51:48 -0400

    Middleware added

--- projectRoute.js
+++ projectRoute.js
@@ -1,67 +1,60 @@
-const express = require("express")
+const express = require("express");
 const projectDb = require("./data/helpers/actionModel");
-const actionsDb = require("./data/helpers/actionModel")
-const projectRouter = express.Router()
+const actionsDb = require("./data/helpers/actionModel");
+const projectRouter = express.Router();
 
-projectRouter.get("/:id", (req, res, next) => {
-    projectDb.get(req.params.id).then((result) => {
-      result
-        ? res.status(200).json(result).end()
-        : res.status(404).json({ message: "Not Found" }).end();
-    });
-  });
+function validateId(req, res, next) {
+  projectDb
+    .get(req.params.id)
+    .then(next())
+    .catch((e) => res.status(404).json({ message: "Not Found" }).end());
+}
 
-  projectRouter.post("/", (req, res, next) => {
-    projectDb
-      .insert(req.body)
-      .then((response) =>
-        response
-          ? res.status(201).json(response).end()
-          : res.status(500).json({ message: "There was an error" }).end()
-      );
-  });
-  projectRouter.post("/:id/actions", (req, res, next) => {
-    const actions = req.body;
-    actions.project_id = req.params.id;
-    projectDb.get(req.params.p_id).then((project) => {
-      project
-        ? actionsDb
-            .insert(actions)
-            .then((action) =>
-              action
-                ? res.status(201).json(action).end()
-                : res.status(500).json({ message: "There was a problem" }).end()
-            )
-        : res.status(404).json({ message: "Project not found" }).end();
-    });
-  });
-  projectRouter.put("/:id", (req, res, next) => {
-    projectDb.get(req.params.id).then((result) => {
-      result
-        ? projectDb
-            .update(req.params.id, req.body)
-            .then((result) =>
-              result
-                ? res.status(201).json(result).end()
-                : res.status(500).json({ message: "Something went wrong" }).end()
-            )
-        : res.status(404).json({ message: "Not Found" }).end();
-    });
-  });
+projectRouter.get("/:id", validateId, (req, res, next) => {
+  projectDb
+    .get(req.params.id)
+    .then((result) => {
+      res.status(200).json(result).end();
+    })
+    .catch((e) => res.status(500).json({ message: "something went wrong" }));
+});
 
-  projectRouter.delete('/:id',(req, res, next)=>{
-    projectDb.get(req.params.id).then((result) => {
-        result
-          ? projectDb
-              .remove(req.params.id)
-              .then((result) =>
-                result === 1
-                  ? res.status(204).json({message: "Item removed"}).end()
-                  : res.status(500).json({ message: "Something went wrong" }).end()
-              )
-          : res.status(404).json({ message: "Not Found" }).end();
-      });
-})
-  
+projectRouter.post("/", (req, res, next) => {
+  projectDb
+    .insert(req.body)
+    .then((response) => res.status(201).json(response).end())
+    .catch((e) =>
+      res.status(500).json({ message: "There was an error" }).end()
+    );
+});
 
-module.exports = projectRouter
+projectRouter.post("/:id/actions", validateId, (req, res, next) => {
+  const actions = req.body;
+  actions.project_id = req.params.id;
+  actionsDb
+    .insert(actions)
+    .then((action) => res.status(201).json(action).end())
+    .catch((e) =>
+      res.status(500).json({ message: "There was a problem" }).end()
+    );
+});
+
+projectRouter.put("/:id", validateId, (req, res, next) => {
+  projectDb
+    .update(req.params.id, req.body)
+    .then((result) => res.status(201).json(result).end())
+    .catch((e) =>
+      res.status(500).json({ message: "Something went wrong" }).end()
+    );
+});
+
+projectRouter.delete("/:id", validateId, (req, res, next) => {
+  projectDb
+    .remove(req.params.id)
+    .then((result) => res.status(204).json({ message: "Item removed" }).end())
+    .catch((err) =>
+      res.status(500).json({ message: "Something went wrong" }).end()
+    );
+});
+
+module.exports = projectRouter;
